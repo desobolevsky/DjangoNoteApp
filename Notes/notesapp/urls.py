6e53e88499c236512f67ad8d9683ab33d48f1d03
desobolevsky <desobolevsky@gmail.com>
2020-08-28 16:30:01 +0300

    add create note functionality

--- Notes/notesapp/urls.py
+++ Notes/notesapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
 from . import views as page_views
 
 urlpatterns = [
     path('', page_views.home_page, name='home-page'),
+    path('create/', page_views.CreateNoteView.as_view(template_name='notesapp/create_note.html'),
+         name='create-note-page')
 ]
--- Notes/notesapp/views.py
+++ Notes/notesapp/views.py
@@ -1,6 +1,26 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from .models import Note
 
 
 # Create your views here.
 def home_page(request):
-    return render(request, 'notesapp/home.html', context=None)
+    if request.user.is_authenticated:
+        notes = request.user.note_set.all()
+        context = {'notes': notes}
+    else:
+        context = None
+    return render(request, 'notesapp/home.html', context=context)
+
+
+class CreateNoteView(LoginRequiredMixin, CreateView):
+    model = Note
+    fields = ['title', 'text']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    success_url = reverse_lazy('home-page')
