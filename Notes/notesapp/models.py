25faf7122092495e1ae289e80749e2f50c615765
desobolevsky <desobolevsky@gmail.com>
2020-09-05 11:51:05 +0300

    add full-featured text area for notes

--- Notes/notesapp/models.py
+++ Notes/notesapp/models.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.models import User
 from django.db import models
+from ckeditor.fields import RichTextField
 
 
 # Create your models here.
 class Note(models.Model):
     title = models.CharField(max_length=75)
-    text = models.TextField(max_length=10000, blank=True)
+    text = RichTextField(max_length=10000, blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
