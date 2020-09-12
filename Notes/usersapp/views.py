dabe932e5975ff52df423fc8c17e2b9128c8f32f
desobolevsky <desobolevsky@gmail.com>
2020-09-12 13:27:47 +0300

    add auto-login after registration

--- Notes/usersapp/views.py
+++ Notes/usersapp/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import authenticate,login
+from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
 from .forms import UserRegistrationForm
 
@@ -7,6 +9,13 @@ def register_page(request):
         reg_form = UserRegistrationForm(request.POST)
         if reg_form.is_valid():
             reg_form.save()
+
+            # login user after registration
+            username = request.POST['username']
+            password = request.POST['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+
             return redirect('home-page')
     else:
         reg_form = UserRegistrationForm()
